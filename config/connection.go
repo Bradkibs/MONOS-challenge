aebef6746720ef7691d82e7920b40bd28ac75f27
bradkibs <bradleykibs@gmail.com>
2024-12-08 11:24:08 +0300

    configuring the db

---
+++ config/connection.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"log"
+)
+
+func Connect() (*pgxpool.Pool, error) {
+	connPool, err := pgxpool.NewWithConfig(context.Background(), Config())
+	if err != nil {
+		log.Fatal("Error while creating connection to the databse!!")
+	}
+	conn, err := connPool.Acquire(context.Background())
+	if err != nil {
+		log.Fatal("Error while acquiring connection from the database pool!!")
+	}
+	defer conn.Release()
+
+	err = conn.Ping(context.Background())
+	if err != nil {
+		log.Fatal("Could not ping the database!!")
+	}
+	log.Println("Connected to the database!!")
+
+	return connPool, err
+}
--- config/database.go
+++ config/database.go
@@ -1,1 +1,56 @@
 package config
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"time"
+)
+
+func Config() *pgxpool.Config {
+	const defaultMaxConns = int32(4)
+	const defaultMinConns = int32(0)
+	const defaultMaxConnLifetime = time.Hour
+	const defaultMaxConnIdleTime = time.Minute * 30
+	const defaultHealthCheckPeriod = time.Minute
+	const defaultConnectTimeout = time.Second * 5
+
+	err := godotenv.Load("config/.env")
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+	databaseUrl := os.Getenv("PG_DB")
+	if databaseUrl == "" {
+		log.Fatal("PG_DB environment variable not set or is empty")
+	}
+	dbConfig, err := pgxpool.ParseConfig(databaseUrl)
+	if err != nil {
+		log.Fatal("Failed to create a config, error: ", err)
+	}
+
+	dbConfig.MaxConns = defaultMaxConns
+	dbConfig.MinConns = defaultMinConns
+	dbConfig.MaxConnLifetime = defaultMaxConnLifetime
+	dbConfig.MaxConnIdleTime = defaultMaxConnIdleTime
+	dbConfig.HealthCheckPeriod = defaultHealthCheckPeriod
+	dbConfig.ConnConfig.ConnectTimeout = defaultConnectTimeout
+
+	dbConfig.BeforeAcquire = func(ctx context.Context, c *pgx.Conn) bool {
+		log.Println("Before acquiring the connection pool to the database!!")
+		return true
+	}
+
+	dbConfig.AfterRelease = func(c *pgx.Conn) bool {
+		log.Println("After releasing the connection pool to the database!!")
+		return true
+	}
+
+	dbConfig.BeforeClose = func(c *pgx.Conn) {
+		log.Println("Closed the connection pool to the database!!")
+	}
+
+	return dbConfig
+}
